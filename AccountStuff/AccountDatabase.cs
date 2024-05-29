363face42f45d7bbba041462421395b558bc1a40
xjosob <jocke.s@windowslive.com>
2024-05-29 14:45:27 +0200

    update

--- AccountStuff/AccountDatabase.cs
+++ AccountStuff/AccountDatabase.cs
@@ -9,7 +9,7 @@ namespace PasswordManager.AccountStuff
 {
     public class AccountDatabase
     {
-        public static string CentralFolderPath = @"E:\PasswordsLocation";
+        public static string CentralFolderPath = @"C:\PasswordsLocation";
 
         public static string AccName = "accName.txt";
         public static string EmailName = "email.txt";
@@ -22,6 +22,43 @@ namespace PasswordManager.AccountStuff
         public static string ExtrIn3Name = "extrin3.txt";
         public static string ExtrIn4Name = "extrin4.txt";
         public static string ExtrIn5Name = "extrin5.txt";
+
+        public static class AccountSaver
+        {
+            public static void SaveFiles(List<AccountStructure> accounts) { SaveFiles(accounts, CentralFolderPath); }
+            public static void SaveFiles(List<AccountStructure> accounts, string directory)
+            {
+                List<string> NewAccName = new List<string>();
+                List<string> NewEmail = new List<string>();
+                List<string> NewUserName = new List<string>();
+                List<string> NewPassword = new List<string>();
+                List<string> NewDateofBirth = new List<string>();
+                List<string> NewSecurityInfo = new List<string>();
+                List<string> NewExtraInfo1 = new List<string>();
+                List<string> NewExtraInfo2 = new List<string>();
+                List<string> NewExtraInfo3 = new List<string>();
+                List<string> NewExtraInfo4 = new List<string>();
+                List<string> NewExtraInfo5 = new List<string>();
+
+                for (int i = 0; i < accounts.Count; i++)
+                {
+                    NewAccName.Add(accounts[i].AccountName);
+                    NewEmail.Add(accounts[i].EmailAdress);
+                    NewUserName.Add(accounts[i].UserName);
+                    NewPassword.Add(accounts[i].Password);
+                    NewDateofBirth.Add(accounts[i].DateOfBirth);
+                    NewSecurityInfo.Add(accounts[i].SecurityInfo);
+                    NewExtraInfo1.Add(accounts[i].ExtraInfo1);
+                    NewExtraInfo2.Add(accounts[i].ExtraInfo2);
+                    NewExtraInfo3.Add(accounts[i].ExtraInfo3);
+                    NewExtraInfo4.Add(accounts[i].ExtraInfo4);
+                    NewExtraInfo5.Add(accounts[i].ExtraInfo5);
+                }
+
+                File.WriteAllLines(Path.Combine(directory, AccName), NewAccName);
+            }
+        }
+
 
         public static void createdirthing()
         {
